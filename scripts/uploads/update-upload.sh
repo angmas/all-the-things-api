6576a23f5616716d94fe607055e72497962ff34a
narichasavanorkejoyce <nari.savanorkejoyce@gmail.com>
2017-05-16 13:54:45 -0400

    Update title of uploaded file
    We can now update the title of a file that has been
    uploaded to AWS. This feature was tested through a
    curl script and it worked.

---
+++ scripts/uploads/update-upload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/uploads"
+ID=591b388fafc10523b3efee8d
+TOKEN=sfq0ufltW9uAO+05vdn7MaeHhJgwz8nbi4+tnUt6Ztg=--zZPhwwgVw01lUFGcFF/548jhcV4nnfT8nvNsemzgpfU=
+TITLE="prarie-pic"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "upload": {
+      "title": "'"${TITLE}"'"
+    }
+  }'
+
+echo

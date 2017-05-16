df88f99cc9adda9bde219c81851db2ff17db766f
jim-moody <jim.moody@libertymutual.com>
2017-05-16 11:17:52 -0400

    Add user auth to upload model and controller
    Adds a user id to the upload schema and stores it in the database
    
    POST method for uploading is done

--- app/models/upload.js
+++ app/models/upload.js
@@ -10,9 +10,22 @@ const uploadSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true
+  },
+  _owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   }
 }, {
-  timestamps: true
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: function (doc, ret, options) {
+      const userId = (options.user && options.user._id) || false
+      ret.editable = userId && userId.equals(doc._owner)
+      return ret
+    }
+  }
 })
 
 uploadSchema.virtual('length').get(function length () {
